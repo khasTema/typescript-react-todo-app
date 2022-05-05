7bddcef474001354e16f547e1d98548ea50a95ea
Artem Khasin <akhasin.dev@gmail.com>
2022-05-05 18:12:10 +0300

    DnD not working with React 18

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,31 @@ const App: React.FC= () => {
   };
 
   const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result
 
+    if (!destination) return;
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    let add,
+        active = toDoS,
+        complete = completedTodos;
+
+    if(source.droppableId === 'TodosList') {
+      add = active[source.index];
+      active.splice(source.index, 1)
+    }else{
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if(destination.droppableId === 'TodosList') {
+      active.splice(destination.index, 0, add)
+    }else{
+      complete.splice(destination.index, 0, add)
+    }
+
+    setCompletedTodos(complete);
+    setToDoS(active);
   }
 
   return ( 
